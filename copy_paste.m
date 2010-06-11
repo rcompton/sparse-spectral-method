92a3b167360c44e976003a2ed77adb70364e62a0
rcompton <rcompton@localhost>
2010-06-10 23:47:47 -0700

    I hate this crap.

--- copy_paste.m
+++ copy_paste.m
@@ -28,8 +28,8 @@ V = (k0 - k2*x.^2 + k3*x.^3 + k4*x.^4).*(x<x1).*(x2<x);
 
 % Define uniform time domain
 % WTF?!
-dt = 5.73 %% Time step (dt < pi/(3*dVmax)...)
-Nt = 16384
+dt = .00573 %% Time step (dt < pi/(3*dVmax)...)
+Nt = 50384
 
 % Define initial wavefunction
 a = 1.9; % shift
@@ -43,122 +43,53 @@ M = 1/2
 % Define spectral vairables
 k = Nx*linspace(-1/2,1/2,Nx);
 
-%%-------------------------------------------------------------------------
-%%
-%%% Potential
-%V0 = 20000;
-%V = zeros(length(x),1) - V0; % 1*((2*x).^2 - (0.6*a)^2);  %  
-%b = a/16;
-%V(x<-b) = 0;
-%V(x>+b) = 0;
-% V(1:5) = V(1:5)-i*1e3;  %% Absorption at simulation boundary
-% V(end-5:end) = V(end-5:end)-i*1e3;  %% Absorption at simulation boundary
-%%%
-%%-------------------------------------------------------------------------
-%%
-%%% initial wave packet
-%% Gaussian
-%Phi0 = exp(-(5*(x-0*a/128)).^2); 
-%
-%% Odd
-% mode = 3;
-% Phi0 = sqrt(2/b)*sin(mode*pi*x/b);
-% Phi0(x<-b) = 0;
-% Phi0(x>+b) = 0;
-%
-%% Even
-% mode = 8;
-% Phi0 = sqrt(2/b)*cos(mode*pi*x/b);
-% Phi0(x<-b) = 0;
-% Phi0(x>+b) = 0;
-%%
-%%-------------------------------------------------------------------------
-%%
-%%
-%figure(1);set(gcf,'position',[37 208 538 732]);
-%title('Potential');
-%plot(x,V,'r');hold on;plot(x,max(abs(real(V)))*abs(Phi0c));hold off; pause(1);
 %% Define the spectral operators.
 % the method in it's pure form applys GK twice
 % but thats' the same as an offset and GK2 once in a loop...
 % why do we shift?
 % I don't know.
-
 GK = fftshift(exp(-(1i*dt/(4*M))*((2*pi/L0)^2)*(k.^2))); %% dt/2 kinetic energy propagator
-
-%GK = exp(-(1i*dt/(4*M))*((2*pi/a)^2)*(k.^2)); %% dt/2 kinetic energy propagator
-%GK2 = exp(-(1i*dt/(2*M))*((2*pi/a)^2)*(k.^2)); %% dt kinetic energy propagator
-
 GV = exp(-1i*dt*V); %% Potential spatial interaction
-
-% plot((-(dt/(4*M))*((2*pi/a)^2)*(k.^2)));
-% plot(-dt*V);
-%%
 
 
 %% The major computational loop
 ipsi = fft(psi0);
 psi = ifft(ipsi.*GK); %propagate dt/2 to get in line with future loop iters...
 psi = GV.*psi;
-NPt = 50000; %How did you pick? dEmin < pi/T is minimum seperation that can resolve.
-Pt = zeros(1,NPt);
-En = -105.99;  %% Energy eigenvalue. what the hell? how???
-T = dt*NPt;
-t = linspace(0,T,NPt);
-%wt = (1-cos(2*pi*t/length(t))); %I don't think I'll need the Hanning window.
-%uns = 0; 
+
+Pt = zeros(1,Nt);
+T = dt*Nt;
+t = linspace(0,T,Nt);
 
 %
 % Propagate all the timesteps
 % ***One timestep per iteration!***
 %
-for nrn = 1:NPt
-    %note, Phi was already propagated dt/2 before the loop.
-    iPhi = fft(Phi);
-    Pt(nrn) = trapz(x,Phi0c.*ifft(iPhi.*GK)); %propagate by dt/2 to line up with paper.
-    Phi = ifft(iPhi.*GK2); %propagate by dt
-%%
-%     %%
-%     %
-%     % Compute the eigenfunctions
-%     % (which I don't care about)
-%     %
-%     unl = Phi*wt(nrn)*exp(1i*En*nrn*dt);
-%     if nrn > 1
-%         una = (unp + unl)*dt/2; %% Trapezoidal area
-%         uns = uns + una;  %% Explicit trapezoidal integration
-%         if mod(nrn,1000) == 0
-%             figure(1);
-%             subplot(4,1,3);plot(x,abs(una)); 
-%             title('\int_t^{t+dt} \Phi_x(t) w(t) exp(i*E_n*t) dt');
-%             xlabel('x');axis tight;
-%             subplot(4,1,4);plot(x,real(uns)); 
-%             title('Eigen function @ E = -105.99');
-%             xlabel('x');ylabel('Amp');axis tight;
-%         end
-%     end
-%     unp = unl;
-%     %%
-%     if mod(nrn,1000) == 0
-%         figure(1);
-%         subplot(4,1,1);plot(x, real(Phi),'r'); 
-%         title(['\Phi_x  t=',num2str(t(nrn))]); 
-%         xlabel('x');ylabel('Amp');axis tight;
-%         subplot(4,1,2);plot(k,fftshift(real(iPhi)),'r-');
-%         title('\Phi_k'); 
-%         xlabel('k');ylabel('k-space Amp');axis tight;
-%         pause(0.2);
-%     end
-%%
-%%
-    Phi = GV.*Phi;    
+psi = psi0;
+ipsi = fft(psi);
+for nrn = 1:Nt
+    % momentum space propagation
+    ipsi = ipsi.*GK;
+    % move into physical space and apply potential operator
+    psi = ifft(ipsi);
+    psi = GV.*psi;
+    % move into momentum space and propagate again
+    ipsi = fft(psi);
+    ipsi = ipsi.*GK;
+    
+    % move back into physical space and record P(t)
+    psi = ifft(ipsi);
+    Pt(nrn) = trapz(x, psi0c.*psi);
+    
+    
 end
-iPhi = fft(Phi);
-Phi = ifft(iPhi.*GK);%line up with paper's timestepping by dt/2 propagation
+
+
+
 %%
 estep = 1;  %% Sampling period
 Po = Pt(1:estep:length(Pt));
-T = dt*NPt;
+T = dt*Nt;
 t = linspace(0,T,length(Po));
 E = (1/dt)*(linspace(-pi,pi,length(Po)));
 %% Draw the energy spectrum with classical FFT methods
