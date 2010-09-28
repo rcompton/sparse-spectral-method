fdd567ac34e2f6629c3653d046a9f94ff5ff7fde
rcompton <rcompton@localhost>
2010-09-27 22:10:26 -0700

    Some words added

--- many_cheby_steps.m
+++ many_cheby_steps.m
@@ -13,8 +13,8 @@ dx = median(diff(x))
 
 
 dt = .5;
-tmax = 1700;
-Nt = tmax/dt;
+tmax = 1941;
+Nt = ceil(tmax/dt);
 
 % re = 3.2889;
 % De = 0.01688;
@@ -31,18 +31,18 @@ Nt = tmax/dt;
 %V = zeros(size(x));
 %V = .7*(x<-L/3) + .8*(x>L/3);
 %V = (2*x).^2;
-V = 5.8*(x< -L/16) + 5.8*(x > L/16);
+V = 2.8*(x< -L/16) + 2.8*(x > L/16);
 V = V';
 
 
 % Wtf?
-M = 50;
+M = 25;
 
 %animation capture setup
-fig1 = figure(1);
-fps = 30;
-axis([0, L, 0, 1]);
-aviobj = avifile('swe1d3.avi', 'FPS', fps);
+%fig1 = figure(1);
+%fps = 30;
+%axis([0, L, 0, 1]);
+%aviobj = avifile('swe1d3.avi', 'FPS', fps);
 
 dE = (pi^2)/(2*M*dx^2) + max(V) - min(V)
 maxE = (pi^2)/(2*M*dx^2) + max(V)
@@ -61,21 +61,25 @@ psi0 = psi0';
 I = trapz(x,psi0.*conj(psi0));
 psi0 = psi0 / sqrt(I);
 
-
 Hnormspec = inline( '(2/dE)*((-1/(2*M))* specdiff(Phi,x) + V.*Phi) - (1+2*minE/dE).*Phi','M', 'V','dE','minE','k','dx','x','Phi');
 
-%figure maxk for a given timestep
-maxk = 2;
 nexttenjays = 1:.3:1.5;
-while max( abs(besselj(maxk,dE*dt*nexttenjays)) ) > 1e-6 %&& abs(besselj(maxk,dt*(jdt+pi))) >1e-6
-    maxk = maxk+1;
-end
 
+%set up a randomized time grid
+num_samples = ceil(Nt/2);
+stream = RandStream('mrg32k3a');
+sample_points = sort(unique([1 randsample(stream,1:Nt, num_samples)]));
+num_samples = length(sample_points);
+
+ts = sample_points*dt;
+dts = diff(ts);
+
+%The big loop
 psis = zeros(n,Nt);
 psis(:,1) = psi0;
+%for nrn = 2:Nt
+for nrn = 2:length(sample_points) 
 
-for nrn = 2:Nt
-      
     %% Do a chebystep
     %make maxk Tkpsis
     Tkpsis = zeros(n,100);
@@ -83,18 +87,23 @@ for nrn = 2:Nt
     %Tkpsis(:,2) = Hnorm(M,V,dE,minE,dx,psi0);
     Tkpsis(:,2) = Hnormspec(M,V,dE,minE,k,dx,x,psis(:,nrn-1));
     kunt = 2;
-    while max( abs(besselj(kunt,dE*dt*nexttenjays)) ) > 1e-6
+    while max( abs(besselj(kunt,dE*dts(nrn-1)*nexttenjays)) ) > 1e-6
         kunt = kunt+1;
         %Tkpsis(:,k) = 2*Hnorm(M,V,dE,minE,dx,Tkpsis(:,k-1)) - Tkpsis(:,k-2);
         Tkpsis(:,kunt) = 2*Hnormspec(M,V,dE,minE,k,dx,x,Tkpsis(:,kunt-1)) - Tkpsis(:,kunt-2);
     end
     
+    if mod(nrn,37)==0
+        fprintf('number of cheby terms: %f \n',kunt);
+        fprintf('size of this step: %f \n',dts(nrn-1));
+        fprintf('percentage of computation done: %f \n', nrn/length(sample_points));
+    end
     
     %make the sum
-    chebsum = besselj(0,dt*dE)*Tkpsis(:,1);
-    chebsum = chebsum + -2i*besselj(1,dt*dE)*Tkpsis(:,2);
+    chebsum = besselj(0,dts(nrn-1)*dE)*Tkpsis(:,1);
+    chebsum = chebsum + -2i*besselj(1,dts(nrn-1)*dE)*Tkpsis(:,2);
     for chebkunt=2:kunt-1
-        chebsum = chebsum + 2*((-1i)^chebkunt)*besselj(chebkunt,dt*dE)*Tkpsis(:,chebkunt+1);
+        chebsum = chebsum + 2*((-1i)^chebkunt)*besselj(chebkunt,dts(nrn-1)*dE)*Tkpsis(:,chebkunt+1);
     end
     
     psis(:,nrn) = chebsum;
@@ -123,37 +132,36 @@ for nrn = 2:Nt
     
 
 end
-close(fig1);
-aviobj = close(aviobj);
+%close(fig1);
+%aviobj = close(aviobj);
 
 
 %% Form PE and get the spectrum...
 % back to old shit
-ts = linspace(0,tmax,length(Pt));
-wt = (1-cos(2*pi*ts/length(ts)));
+ts_uniform = linspace(0,tmax,Nt);
+%wt = (1-cos(2*pi*ts/length(ts)));
 
 estep = 1;  %% Sampling period
 Po = Pt(1:estep:length(Pt));
-E = (1/dt)*(linspace(-pi,pi,length(Pt)));
+E = (1/dt)*(linspace(-pi,pi,Nt));
 Po = (1-cos(2*pi*ts/tmax)).*Po;
 
-Pe = fft(Po);
-Pe = fftshift(Pe);
+%Pe = fft(Po);
+%Pe = fftshift(Pe);
 
-figure();
-plot(E,abs(Pe));
+%figure();
+%plot(E,abs(Pe));
 
 %% Practice points for the L1 reconstruction.
 %seed the random generator with the example seeder
-stream = RandStream('mrg32k3a');
+%stream = RandStream('mrg32k3a');
 
-
-num_samples = Nt/5;
+%num_samples = Nt/10;
 
 %make our downsampling matrix
-sample_points = sort(randsample(stream,1:Nt, num_samples));
 
 % FPC_AS A_operator class
+% !!! This is where the locations of the samples appear !!!
 A = A_operator( @(z) pifft(z,sample_points), @(z) pfft(z,sample_points,Nt) );
 
 % tiny mu corresponds to heavy weight on the fidelity term
@@ -169,15 +177,12 @@ mu = 1e-10;
 % u_samples = transpose(u_samples);
 %Pe = usparse;
 
+%for practice sampling from Po
+%u_samples = transpose(Po(sample_points)*sqrt(Nt));
 
-
-
-u_samples = transpose(Po(sample_points)*sqrt(length(Po)));
-
-%fprintf('u-Po %f\n',norm(u - Po));
 % Call Wotao's code.
-%[Pe_new, Out] = FPC_AS(n, A, u_samples, mu);
-
+%[uHat_approx, Out] = FPC_AS(Nt, A, u_samples, mu);
+u_samples = transpose(Po*sqrt(Nt));
 [uHat_approx, Out] = FPC_AS(Nt, A, u_samples, mu);
 
 uHat_approx = fftshift(uHat_approx); %Why?
@@ -185,10 +190,10 @@ uHat_approx = fftshift(uHat_approx); %Why?
 close all;
 figure()
 hold on;
-plot(E,abs(Pe));
-plot(E,abs(uHat_approx),'r--');
+%plot(abs(Pe));
+plot(abs(uHat_approx),'r--');
 
 %% Neat picture
 figure();
-imshow(abs(psis(min(find(-L/9<x)):max(find(x<L/9)), :)))
+imshow(abs(psis(find(-L/9<x, 1 ):find(x<L/9, 1, 'last' ), 1:num_samples)))
 colormap hot;
